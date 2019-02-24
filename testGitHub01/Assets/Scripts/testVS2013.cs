d370c1a0972fffa29ef9d1a12e3f8cc156210325
HenryZ <henrystriker8@gmail.com>
2019-02-24 23:49:11 +0800

    test VS2017

---
+++ testGitHub01/Assets/Scripts/testVS2013.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testVS2013 : MonoBehaviour
+{
+    Transform _transform;
+    public float speed = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("aaaaaaaaaaaaaaa");
+        _transform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _transform.Rotate(Vector3.up * speed);
+    }
+}
